6924dad5eac70f52d87796cd8b2690fe4a57f9c8
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-06 22:00:17 -0400

    Create .__init__.py

---
+++ sub-domainer_V2/.__init__.py
@@ -0,0 +1,7 @@
+"""
+Initialization file for the package.
+
+This file marks the directory as a Python package and imports the LLMAnalyzer class for easy access.
+"""
+
+from .llm_analyzer import LLMAnalyzer
